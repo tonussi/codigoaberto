37b637b89cbd2cff3ab7ad41858af7642d6b02fb
tonussi <lptonussi@gmail.com>
2014-04-11 08:56:45 -0300

    Relaciona amigo e suas informacoes e adiciona fonte monospace

--- Teia/src/teia/Teia.java
+++ Teia/src/teia/Teia.java
@@ -4,38 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.Amigo;
+import model.Aresta;
+import model.Info;
+import model.Nodo;
 import processing.core.PApplet;
+import processing.core.PFont;
 import dao.AmigoDataAccessObject;
 
-@SuppressWarnings("serial")
 public class Teia extends PApplet {
 
 	List<Amigo> amigos;
-	AmigoDataAccessObject amigodao;
+	List<Info> infos;
+	List<Aresta> arestas;
+	List<Nodo> nodos;
+	AmigoDataAccessObject dao;
+	PFont font;
 
 	public Teia() {
+		font = createFont("Monospace", 6);
+
+		infos = new ArrayList<Info>();
+
 		amigos = new ArrayList<Amigo>();
-		amigodao = new AmigoDataAccessObject(this, "lucastonussi", "lucastonussi",
-				"hung4ro5");
-		System.out.println(amigodao.quantidadeAmigos());
-		amigos = amigodao.listadeAmigos();
+
+		dao = new AmigoDataAccessObject(this, font, "lucastonussi",
+				"lucastonussi", "hung4ro5");
+
+		infos = dao.listadeAmigos();
+
+		for (Info info : infos)
+			amigos.add(new Amigo(this, info));
+
+		arestas = new ArrayList<Aresta>();
+
+		arestas.add(new Aresta(this, 40, 20, 40));
 	}
 
 	@Override
 	public void setup() {
-		size(350, 200);
-		background(255);
+		// size(3840, 2160);
+		size(350, 350);
 		translate(width / 2, height / 2);
 	}
 
 	@Override
 	public void draw() {
-		background(255);
+		background(43);
 		translate(width / 2, height / 2);
 		for (Amigo amigo : amigos) {
 			amigo.display();
 			amigo.mova();
 		}
+
 	}
 
 	public static void main(String _args[]) {
