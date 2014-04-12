1e27acd18c561ce5871dfb2266fb226808b95347
tonussi <lptonussi@gmail.com>
2014-04-12 12:51:10 -0300

    Torna amigo numa abstracao

--- Teia/src/model/Amigo.java
+++ Teia/src/model/Amigo.java
@@ -3,22 +3,21 @@ package model;
 import processing.core.PApplet;
 import processing.core.PVector;
 
-public class Amigo {
+public abstract class Amigo {
 
-	private Info info;
-
-	private PVector local, dir, acel;
+	protected Info info;
+	protected PVector local, dir, acel;
 	PApplet processing;
 
 	public Amigo(PApplet processing, Info info) {
+
 		this.processing = processing;
+		this.info = info;
 
 		local = new PVector(processing.random(processing.width),
 				processing.random(processing.height));
 		dir = PVector.random2D();
 		acel = PVector.random2D();
-
-		this.info = info;
 	}
 
 	public void mova() {
@@ -27,22 +26,5 @@ public class Amigo {
 	}
 
 	public void display() {
-		processing.pushMatrix();
-		processing.translate(local.x, local.y);
-
-		processing.fill(255);
-
-		processing.triangle(local.x, local.y + 3, local.x + 3, local.y,
-				local.x - 3, local.y);
-
-		processing.stroke(255);
-		processing.strokeWeight(2);
-		processing.noFill();
-
-		processing.ellipse(local.x, local.y, 20, 20);
-
-		info.adicionaInformacao(local.x, local.y);
-
-		processing.popMatrix();
 	}
 }
